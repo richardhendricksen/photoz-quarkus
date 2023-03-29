c9c1d783ecf8213df733f396d26767542364c4f6
Richard Hendricksen <richard.hendricksen@gmail.com>
2023-03-29 15:54:39 +0200

    add delete and create endpoints

--- src/main/java/nl/codecontrol/rest/PhotozResource.java
+++ src/main/java/nl/codecontrol/rest/PhotozResource.java
@@ -3,13 +3,13 @@ package nl.codecontrol.rest;
 import nl.codecontrol.model.Photo;
 import org.jboss.resteasy.annotations.jaxrs.PathParam;
 
-import javax.ws.rs.GET;
-import javax.ws.rs.Path;
-import javax.ws.rs.Produces;
+import javax.ws.rs.*;
 import javax.ws.rs.core.MediaType;
 import java.util.Collection;
 import java.util.HashMap;
 import java.util.Map;
+
+import static java.util.Objects.isNull;
 
 @Path("/photoz")
 public class PhotozResource {
@@ -28,6 +28,34 @@ public class PhotozResource {
     @Path("/{id}")
     @Produces(MediaType.APPLICATION_JSON)
     public Photo get(@PathParam("id") long id) {
-        return db.get(id);
+        final Photo photo = db.get(id);
+        if (isNull(photo)) {
+            throw new NotFoundException("Unknown photo: %s".formatted(id));
+        }
+
+        return photo;
+    }
+
+    @DELETE
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public Photo delete(@PathParam("id") long id) {
+        final Photo photo = db.remove(id);
+        if (isNull(photo)) {
+            throw new NotFoundException("Unknown photo: %s".formatted(id));
+        }
+
+        return photo;
+    }
+
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Produces(MediaType.APPLICATION_JSON)
+    public Photo create(Photo photo) {
+        long id = db.keySet().stream().max(Long::compare).get() + 1;
+        photo.setId(id);
+        db.put(photo.getId(), photo);
+
+        return photo;
     }
 }
