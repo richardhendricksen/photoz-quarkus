613770f7e95c4c1200e203a7fe9721aafb1de2db
Richard Hendricksen <richard.hendricksen@gmail.com>
2023-04-05 21:23:15 +0200

    cleanup MultiPartHelper

--- src/main/java/nl/codecontrol/util/MultiPartHelper.java
+++ src/main/java/nl/codecontrol/util/MultiPartHelper.java
@@ -4,32 +4,29 @@ import org.jboss.resteasy.plugins.providers.multipart.InputPart;
 import org.jboss.resteasy.plugins.providers.multipart.MultipartFormDataInput;
 
 import javax.enterprise.context.ApplicationScoped;
-import javax.ws.rs.core.MultivaluedMap;
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 
 @ApplicationScoped
 public class MultiPartHelper {
 
     public MultiPartFile extractData(MultipartFormDataInput input) throws IOException {
-        String fileName = "";
+        List<InputPart> inParts = input.getFormDataMap().get("data");
+        if (inParts == null || inParts.isEmpty())
+            throw new IllegalStateException("No data found");
 
-        final Map<String, List<InputPart>> formParts = input.getFormDataMap();
-        List<InputPart> inPart = formParts.get("data");
-
-        InputPart inputPart = inPart.stream().findFirst().orElseThrow();
+        var inPart = inParts.get(0);
 
         // Retrieve headers, read the Content-Disposition header to obtain the original name of the file
-        MultivaluedMap<String, String> headers = inputPart.getHeaders();
-        String[] contentDispositionHeader = headers.getFirst("Content-Disposition").split(";");
-        for (String name : contentDispositionHeader) {
-            if ((name.trim().startsWith("filename"))) {
-                String[] tmp = name.split("=");
-                fileName = tmp[1].trim().replaceAll("\"", "");
-            }
-        }
+        var headers = inPart.getHeaders();
+        var contentDispositionHeader = headers.getFirst("Content-Disposition").split(";");
+        var fileName = Arrays.stream(contentDispositionHeader)
+            .filter(i -> i.trim().startsWith("filename"))
+            .findFirst()
+            .orElseThrow()
+            .split("=")[1].trim().replaceAll("\"", "");
 
-        return new MultiPartFile(fileName, inputPart.getBody(byte[].class, null), inputPart.getMediaType());
+        return new MultiPartFile(fileName, inPart.getBody(byte[].class, null), inPart.getMediaType());
     }
 }
